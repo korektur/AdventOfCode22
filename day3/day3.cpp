efe68047f3e0064c4b869feac6d890004fcf478b
Ruslan Akhundov <akhundov2@gmail.com>
2022-12-03 11:27:15 +0000

    day3

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -12,14 +12,14 @@ constexpr inline size_t charToNum(const char &c) {
 int main() {
     ifstream infile("day3/day3.in");
     string line;
-    size_t score = 0, score2 = 0, c, lineNum = 0;
+    size_t score = 0, score2 = 0, lineNum = 0, i, c;
     bool visited[52];
     size_t cnt[52];
+    fill_n(cnt, 52, 0);
     while (getline(infile, line)) {
         //part1
         fill_n(visited, 52, false);
-        size_t i = 0;
-        for (; i < line.size() / 2; ++i) visited[charToNum(line[i])] = true;
+        for (i = 0; i < line.size() / 2; ++i) visited[charToNum(line[i])] = true;
 
         for (; i < line.size(); ++i) {
             c = charToNum(line[i]);
@@ -30,21 +30,17 @@ int main() {
         }
 
         //part2
-        if (lineNum++ % 3 == 0) {
-            fill_n(cnt, 52, 0);
-        }
-        fill_n(visited, 52, false);
         for (i = 0; i < line.length(); ++i) {
             size_t j = charToNum(line[i]);
-            if (!visited[j] && ++cnt[j] == 3) score2 += j + 1;
-            visited[j] = true;
+            if (cnt[j] == lineNum - 1 && ++cnt[j] == 3) {
+                score2 += j + 1;
+                lineNum = 0;
+                fill_n(cnt, 52, 0);
+                break;
+            }
         }
     }
 
     cout << "Answer1: " << score << endl;
     cout << "Answer2: " << score2 << endl;
-}
-
-void solvePart1() {
-
 }
