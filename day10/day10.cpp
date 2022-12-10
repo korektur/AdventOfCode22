c602e785489e11b8330b3e005ba309bc252cfca8
Ruslan Akhundov <akhundov2@gmail.com>
2022-12-10 09:50:08 +0000

    day10 part2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -8,18 +8,24 @@ int x = 1;
 int ans = 0;
 int iteration = 0;
 int crt = 0;
+string crtLine;
 
 void incrementIteration() {
-    if (++iteration == 20 || (iteration - 20) % 40 == 0) {
-        ans += x * iteration;
-//        cout << iteration << " " << ans << " " << x << endl;
+    crtLine += abs(crt++ - x) <= 1 ? "#" : ".";
+    if (crtLine.size() == 40) {
+        crt = 0;
+        cout << crtLine << endl;
+        crtLine = string();
+        crtLine.reserve(40);
     }
+    if (++iteration == 20 || (iteration - 20) % 40 == 0) ans += x * iteration;
 }
 
 int main() {
     ifstream infile("day10/day10.in");
     string line;
-    while(getline(infile, line)) {
+    crtLine.reserve(40);
+    while (getline(infile, line)) {
         if (line[0] == 'n') {
             incrementIteration();
         } else {
